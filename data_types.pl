cf7315a888e03e9376cff499474f887a18bf8419
poojitha-lagidi <poojithalagidi.de@gmail.com>
2024-11-30 18:31:41 +0100

    data_types

---
+++ data_types.pl
@@ -0,0 +1,51 @@
+#!D:\PERL\perl\bin -w #shebang line
+
+#Description :: First Perl Program
+#Author :: Poojitha Lagidi
+
+use strict;
+
+=comment
+this is a block comment.
+the identifier (here comment) is used to indicate the start of the comment. identifier can be anything but it is better to stick to standard identifiers like begin comment, for comment, etc.
+(=cut) is only used to terminate the section
+# is used for single line comments
+=cut
+
+#Scalars
+my $var1 = 10;
+my $var2 = 0x15;
+my $var3 = 0.105;
+my $var4 = 'P';
+my $var5 = "Perl";
+print ("$var1 \n$var2 \n$var3 \n$var4 \n$var5");
+
+
+#Arrays
+my @arr1 = (1,2,3);
+my @arr2 = (2,4,6..10); #using range operator
+my @arr3 = ("a", "b", "c");
+my @arr4 = ("a", "b".."e"); #using range operator
+print ("@arr1 \n@arr2 \n@arr3 \n@arr4 \n");
+my @arr5 = qw(This is a quote-word); #quote word
+print"$arr5[3]\n"; # using $ because we are printing a scalar values
+my @arr6 = qw(a b c d e);
+print "$arr6[-1] \t$arr6[-3] \t$arr6[3]\n";
+my @arr7 = (3, "Perl", 4.01); #Combination of different scalars
+my @arr8 = (@arr7, "Perl2.0", 0x27); #Concatenation of arrays
+print "@arr7 \n@arr8 \n";
+#Array Operations
+push @arr8, 3.14;
+print("@arr8\n");
+pop @arr8;
+print("@arr8\n");
+unshift (@arr8, 0x25);
+print("@arr8\n");
+shift @arr8;
+print("@arr8\n");
+
+#Hash
+my %hash1 = (apple => "red", banana => "yellow", clementinen => "orange");
+print "$hash1{apple} \t$hash1{banana} \t$hash1{clementinen}\n"; #o/p: red yellow orange
+$hash1{apple} = "green";
+print "$hash1{apple}"; #o/p: green
